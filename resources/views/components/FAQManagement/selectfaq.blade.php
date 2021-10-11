aea8cf76381b420888ebe23505569b3a3e1aef3b
Tanutchakorn <tanutchakornkhaisaeng@gmail.com>
2021-10-11 20:44:19 +0700

    show faq

--- resources/views/components/FAQManagement/selectfaq.blade.php
+++ resources/views/components/FAQManagement/selectfaq.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-5 mb-5">
     <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <button class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" type="button" id="dropdownMenuButton" data-toggle="dropdown">
             FAQ List
         </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
--- resources/views/components/FAQManagement/showfaq.blade.php
+++ resources/views/components/FAQManagement/showfaq.blade.php
@@ -2,5 +2,7 @@
     <div class="row">
         <h1>{{$faq->title}}</h1>
     </div>
-    <div class="row">{{$faq->detail}}</div>
+    <div class="row">
+        {!!$faq->detail!!}
+    </div>
 </div>
